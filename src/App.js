3da6e6c1240e8d3d9b2ae326ffb7182a4bdd9e3b
SeonHyeonWook <seonhyunuk@gmail.com>
2025-05-30 00:50:48 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>KAU Website</h1>
+      <p>환영합니다!</p>
+    </div>
+  );
+}
+
+export default App;
