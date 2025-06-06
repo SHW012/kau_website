7e75ac74022bc65ab8ff790936a957fd9a9ce1cb
SeonHyeonWook <seonhyunuk@gmail.com>
2025-06-06 22:58:36 +0900

    UI전반적으로 수정

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ import Gallery from "./pages/community/Gallery";
 import QnA from "./pages/community/QnA";
 import NoticeDetail from "./pages/community/NoticeDetail";
 
+// CommunityLayout import
+import CommunityLayout from "./pages/community/CommunityLayout";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
@@ -38,27 +41,91 @@ function AppContent() {
   return (
     <>
       <GlobalStyle />
-      <Navbar isMobile={isMobile} /> {/* 모바일 여부를 prop으로 전달 (선택) */}
+      <Navbar isMobile={isMobile} />
       <div>
         <Routes>
+          {/* 로그인/회원가입 */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+
+          {/* 메인 랜딩 페이지 */}
           <Route path="/" element={<LandingPage />} />
+
+          {/* About 페이지 */}
           <Route path="/about/intro" element={<Intro />} />
           <Route path="/about/greeting" element={<Greeting />} />
           <Route path="/about/team" element={<Team />} />
           <Route path="/about/location" element={<Location />} />
+
+          {/* Programs 페이지 */}
           <Route path="/programs/microdegree" element={<Microdegree />} />
           <Route path="/programs/wemeet" element={<Wemeet />} />
           <Route path="/programs/intern" element={<Intern />} />
+
+          {/* Apply 페이지 */}
           <Route path="/apply/form" element={<Form />} />
           <Route path="/apply/my" element={<My />} />
-          <Route path="/community/notice" element={<Notice />} />
-          <Route path="/community/notice/:id" element={<NoticeDetail />} />
-          <Route path="/community/resources" element={<Resources />} />
-          <Route path="/community/media" element={<Media />} />
-          <Route path="/community/gallery" element={<Gallery />} />
-          <Route path="/community/qna" element={<QnA />} />
+
+          {/* Community 섹션: CommunityLayout으로 래핑 */}
+
+          {/* 1) 공지사항 목록 */}
+          <Route
+            path="/community/notice"
+            element={
+              <CommunityLayout activeKey="공지사항">
+                <Notice />
+              </CommunityLayout>
+            }
+          />
+          {/* 1-1) 공지사항 상세 */}
+          <Route
+            path="/community/notice/:id"
+            element={
+              <CommunityLayout activeKey="공지사항">
+                <NoticeDetail />
+              </CommunityLayout>
+            }
+          />
+
+          {/* 2) 자료실 */}
+          <Route
+            path="/community/resources"
+            element={
+              <CommunityLayout activeKey="자료실">
+                <Resources />
+              </CommunityLayout>
+            }
+          />
+
+          {/* 3) 홍보자료 */}
+          <Route
+            path="/community/media"
+            element={
+              <CommunityLayout activeKey="홍보자료">
+                <Media />
+              </CommunityLayout>
+            }
+          />
+
+          {/* 4) 갤러리 */}
+          <Route
+            path="/community/gallery"
+            element={
+              <CommunityLayout activeKey="갤러리">
+                <Gallery />
+              </CommunityLayout>
+            }
+          />
+
+          {/* 5) QnA */}
+          <Route
+            path="/community/qna"
+            element={
+              <CommunityLayout activeKey="QnA">
+                <QnA />
+              </CommunityLayout>
+            }
+          />
         </Routes>
       </div>
     </>
